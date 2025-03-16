b9d574fefc92acd313eb7f61ce05c7e0527b14f6
Crypto-Kay <Halerd@hotmail.co.uk>
2025-03-16 11:13:57 +0000

    Another prompt

--- Outputs/Outputs2.js
+++ Outputs/Outputs2.js
@@ -3,5 +3,6 @@ window.loadedOutputs = [
     "In a hole in the ground, there lived a hobbit.",
     "To be or not to be, that is the question.",
     "It was the best of times, it was the worst of times.",
-    "The only limit to our realization of tomorrow is our doubts of today."
+    "The only limit to our realization of tomorrow is our doubts of today.",
+    "Better to be safe than sorry"
 ];
